3a4d5b6b44b0111a0e51cb12337e74d8954ddd6a
Arif-TheHuman <22FTT2520@student.pb.edu.bn>
2024-04-15 22:31:53 +0800

    Added profile

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,48 @@
 <template>
-  <p class="text-3xl font-bold">
-    The edit works!
-  </p>
+  <div class="profile">
+    <img class="profile-picture" :src="profilePicture" alt="Profile Picture">
+    <h1 class="profile-name">{{ name }}</h1>
+    <h2 class="profile-hobbies-title">Hobbies</h2>
+    <ul class="profile-hobbies">
+      <li v-for="(hobby, index) in hobbies" :key="index">{{ hobby }}</li>
+    </ul>
+  </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const profilePicture = ref('@/assets/profile.jpeg');
+const name = ref('John Doe');
+const hobbies = ref(['Coding', 'Reading', 'Traveling']);
 </script>
+
+<style scoped>
+.profile {
+  text-align: center;
+}
+
+.profile-picture {
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  margin-top: 20px;
+}
+
+.profile-name {
+  font-size: 2em;
+  font-weight: bold;
+  margin-top: 20px;
+}
+
+.profile-hobbies-title {
+  font-size: 1.5em;
+  margin-top: 20px;
+}
+
+.profile-hobbies {
+  list-style: none;
+  padding: 0;
+  font-size: 1em;
+}
+</style>
